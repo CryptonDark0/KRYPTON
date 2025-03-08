cadcf421e18ca81808c30e0cdf588813af510d38
CryptonDark0 <98961170+CryptonDark0@users.noreply.github.com>
2025-03-08 11:30:24 +1100

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to KRYPTON Frontend!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
